8841ed373359f67f5aa138505ffd7c197f5e08f4
Italo A <italoaurelior@gmail.com>
2023-05-13 23:01:00 -0300

    feat: global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // this will remove any properties that are not in the DTO
+    }),
+  ); // this is the default pipe
   await app.listen(3333);
 }
 bootstrap();
